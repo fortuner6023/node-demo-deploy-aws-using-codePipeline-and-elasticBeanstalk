059769089a8e9b0bfa21fab14661efd32203d049
FORTUNER6023 <Naveen Saini>
2021-03-30 03:55:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send({
+    statusCode: 200,
+    status: "Success",
+    Message: "Welcome To Aws Node Demo",
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
